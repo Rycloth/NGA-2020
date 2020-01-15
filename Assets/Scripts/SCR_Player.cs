07b2e3849b218e8b113c9d9511e8be77804be240
Roland <ryclothgames@gmail.com>
2020-01-15 16:36:47 +0100

    Added Player Scripts
    and working on player

--- Assets/Scripts/SCR_Player.cs
+++ Assets/Scripts/SCR_Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SCR_Dummy : MonoBehaviour
+public class SCR_Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
