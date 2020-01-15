08e1c0c951a11c3373db6c24d312bcdcc5314426
Roland <ryclothgames@gmail.com>
2020-01-15 18:21:24 +0100

    Added Player with Move Function

---
+++ Assets/Resources/Input/InputControls.cs
@@ -0,0 +1,219 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Resources/Input/InputControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputControls : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public @InputControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""41b15f8d-b7d9-4f72-84bf-e02b405ff5af"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""142a3f5c-c458-4e46-ae14-8cf83be3f893"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""1fe6b96d-2347-46e5-9571-039c352d6f2c"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Press to Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""83d6b4cc-952d-475e-ac8b-f9cbf9e0d012"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Hold to Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""c99baa36-9070-4f13-8875-48bc19d28cfa"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""b2102b57-dfa1-4f41-a287-9072175d2ca9"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9f84b445-d6e2-446e-b712-728b0333e6cb"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c32b44a6-e858-4645-9103-cdf480dbeaa7"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Press to Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""12d31b8e-7941-43f3-91a6-a7778c07eee4"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Hold to Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_PresstoRun = m_Player.FindAction("Press to Run", throwIfNotFound: true);
+        m_Player_HoldtoRun = m_Player.FindAction("Hold to Run", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_PresstoRun;
+    private readonly InputAction m_Player_HoldtoRun;
+    public struct PlayerActions
+    {
+        private @InputControls m_Wrapper;
+        public PlayerActions(@InputControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @PresstoRun => m_Wrapper.m_Player_PresstoRun;
+        public InputAction @HoldtoRun => m_Wrapper.m_Player_HoldtoRun;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @PresstoRun.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPresstoRun;
+                @PresstoRun.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPresstoRun;
+                @PresstoRun.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPresstoRun;
+                @HoldtoRun.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnHoldtoRun;
+                @HoldtoRun.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnHoldtoRun;
+                @HoldtoRun.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnHoldtoRun;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @PresstoRun.started += instance.OnPresstoRun;
+                @PresstoRun.performed += instance.OnPresstoRun;
+                @PresstoRun.canceled += instance.OnPresstoRun;
+                @HoldtoRun.started += instance.OnHoldtoRun;
+                @HoldtoRun.performed += instance.OnHoldtoRun;
+                @HoldtoRun.canceled += instance.OnHoldtoRun;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnPresstoRun(InputAction.CallbackContext context);
+        void OnHoldtoRun(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/SCR_Player.cs
+++ Assets/Scripts/SCR_Player.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class SCR_Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	#region Enable & Disable
+	private void OnEnable()
+	{
+		controls.Enable();
+	}
+	private void OnDisable()
+	{
+		controls.Disable();
+	}
+	#endregion
+	#region References
+	[Header("Variables:")]
+	public SCR_Variables variables;
+	InputControls controls;
+	#endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	#region Local Variables
+	float movementSpeed = 3;
+	bool running;
+	#endregion
+
+	private void Awake()
+	{
+		//REF:
+		controls = new InputControls();
+
+		//Functions:
+		InputActions();
+	}
+
+	private void Update()
+	{
+		//Functions
+		Move();
+	}
+
+	void Move()
+	{
+		Vector2 input = controls.Player.Movement.ReadValue<Vector2>();
+
+		if (input != Vector2.zero)
+		{
+			transform.eulerAngles = Vector3.up * Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg;
+		}
+
+		movementSpeed = running ? variables.runSpeed : variables.walkSpeed;
+		movementSpeed *= input.magnitude;
+
+		transform.Translate(transform.forward * movementSpeed * Time.deltaTime, Space.World);
+	}
+
+	void Jump()
+	{
+		print("I am Jumping");
+	}
+
+	void InputActions()
+	{
+		//Set Jump:
+		controls.Player.Jump.performed += ctx => Jump();
+
+		//Check and Set Running:
+		if (variables.holdToRun)
+			controls.Player.HoldtoRun.performed += ctx => running = !running;
+		else
+			controls.Player.PresstoRun.performed += ctx => running = !running;
+	}
 }
