5c810a2e453291c77da102c609917d054c8c2d96
Roland <ryclothgames@gmail.com>
2020-01-16 20:36:08 +0100

    Tried Player _V2 (still sucks)

---
+++ Assets/Scripts/Player/SCR_PlayerV2.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_PlayerV2 : MonoBehaviour
+{
+	#region Enable & Disable
+	private void OnEnable()
+	{
+		controls.Enable();
+	}
+	private void OnDisable()
+	{
+		controls.Disable();
+	}
+	#endregion
+	#region References
+	public SCR_Variables variables;
+	InputControls controls;
+	Rigidbody rb;
+	#endregion
+
+	#region Local Variables
+	bool running;
+	float currentSpeed;
+	float rotationVelocity, speedVelocity;
+	Transform camT;
+	#endregion
+
+	private void Awake()
+	{
+		//REF:
+		camT = GameObject.FindGameObjectWithTag("PlayerCam").transform;
+		controls = new InputControls();
+		rb = GetComponent<Rigidbody>();
+
+		//Functions:
+		InputActions();
+	}
+
+	private void FixedUpdate()
+	{
+		//Functions
+		Move();
+	}
+
+	void Move()
+	{
+		//Get Player Input:
+		Vector2 input = controls.Player.Movement.ReadValue<Vector2>();
+
+		//If Input then set Target Rotation & Smoothly Rotate in Degrees:
+		if (input != Vector2.zero)
+		{
+			float targetRotation = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + camT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, variables.playerTurnSpeed);
+		}
+
+		//Check if Running & Set Speed Accordingly:
+		float targetSpeed = (running ? variables.runSpeed : variables.walkSpeed) * input.magnitude;
+		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, variables.Acceleration);
+
+		//Move the Player:
+		Vector3 velocity = transform.forward * currentSpeed;
+		rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
+	}
+
+	void Jump()
+	{
+		rb.velocity = new Vector3(0, 6, 0);
+	}
+
+	void InputActions()
+	{
+		//Set Jump:
+		controls.Player.Jump.performed += ctx => Jump();
+
+		//Check and Set Running:
+		if (variables.holdToRun)
+			controls.Player.HoldtoRun.performed += ctx => running = !running;
+		else
+			controls.Player.PresstoRun.performed += ctx => running = !running;
+	}
+}
--- Assets/Scripts/Test/SCR_SimpleRot2.cs
+++ Assets/Scripts/Test/SCR_SimpleRot2.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SCR_SimpleRotate : MonoBehaviour
+public class SCR_SimpleRot2 : MonoBehaviour
 {
+	Rigidbody rb;
+
     [Tooltip("Degrees to rotate per second (in EulerAngles)")]
     public Vector3 rotation;
     [Tooltip("Rotate in world or local space")]
     public Space rotationSpace = Space.Self;
 
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
 	void FixedUpdate()
     {
-        transform.Rotate(rotation * Time.deltaTime, rotationSpace);
+        //transform.Rotate(rotation * Time.deltaTime, rotationSpace);
     }
 }
